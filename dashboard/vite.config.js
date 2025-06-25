2b4fb903d1e08edecaa319c70771e4efe5aae440
princeren-36 <renatoperas36@gmail.com>
2025-06-25 21:43:45 +0800

    test host

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
-})
-  
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    https: {
+      key: fs.readFileSync('./localhost-key.pem'),
+      cert: fs.readFileSync('./localhost-cert.pem'),
+    },
+  },
+});
