8ff52929a5b7946b92a8ab98eb661dab7a5a5aa3
YourGitHubUsername <delossantosyowcam@gmail.com>
2025-06-25 09:16:54 +0800

    add

--- dashboard/src/utils/auth.js
+++ dashboard/src/utils/auth.js
@@ -1,4 +1,3 @@
-// Utility to get the current logged-in user from either localStorage or sessionStorage
 export function getCurrentUser() {
   let user = localStorage.getItem("user");
   if (!user) user = sessionStorage.getItem("user");
