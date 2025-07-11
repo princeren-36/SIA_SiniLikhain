52f73d3cf5af62d3156029350b7b010d65895401
princeren-36 <renatoperas36@gmail.com>
2025-07-11 09:48:38 +0800

    Update API_BASE to use main host server and enhance user login to support email

--- server/routes/user.js
+++ server/routes/user.js
@@ -95,7 +95,14 @@ router.post("/verify-registration-otp", async (req, res) => {
 
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
-  const user = await User.findOne({ username, password });
+  // Find user by username OR email
+  const user = await User.findOne({
+    $or: [
+      { username: username },
+      { email: username }
+    ],
+    password
+  });
 
   if (!user) return res.status(401).json({ message: "Invalid credentials" });
 
