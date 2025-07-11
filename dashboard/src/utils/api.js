bb043f7279a848b8a0c76715873a01f149352585
princeren-36 <renatoperas36@gmail.com>
2025-07-11 10:19:18 +0800

    Enhance BuyerProfile component to fetch and display user orders, add refresh functionality, and redirect to login if not authenticated. Update API_BASE to correct server URLs.

--- dashboard/src/utils/api.js
+++ dashboard/src/utils/api.js
@@ -1,7 +1,6 @@
-
-//const API_BASE = "http://192.168.9.48:5000";// = main host server - school
-const API_BASE = "http://192.168.9.46:5000"; // = frontend host server - school
-
+//const API_BASE = "http://localhost:5000"; // = local host server
+const API_BASE = "http://192.168.9.48:5000";// = main host server - school
+//const API_BASE = "http://192.168.9.46:5000"; // = frontend host server - school
 //const API_BASE = "http://192.168.100.15:5000"; // = rentao server
 //const API_BASE = "http://192.168.1.241:5000"; // = marc pogi server
 //const API_BASE = "http://192.168.9.47:5000"; // = frontend host server - school
